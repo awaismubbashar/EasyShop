f586049a31293242a0db607b0598400b5af02370
Awais Mubbashar <awaismubbashar@gmail.com>
2025-06-19 20:05:36 +0500

    Added HeaderView and BannerView in Home screen. Created Header ui.

---
+++ app/src/main/java/com/example/easyshop/ui/components/HeaderView.kt
@@ -0,0 +1,68 @@
+package com.example.easyshop.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.FirebaseFirestore
+
+@Composable
+fun HeaderView(modifier: Modifier) {
+    var name by remember { mutableStateOf("") }
+    val firestore = FirebaseFirestore.getInstance()
+
+    LaunchedEffect(Unit) {
+        val uid = Firebase.auth.currentUser?.uid
+        uid?.let {
+            firestore.collection("users")
+                .document(it).get()
+                .addOnCompleteListener { task ->
+                    name = task.result.get("name").toString().split(" ")[0]
+                }
+        }
+    }
+
+    Row {
+        Column {
+            Text(
+                "Welcome Back", style = TextStyle(
+                    fontSize = 14.sp
+                )
+            )
+            Spacer(Modifier.height(6.dp))
+            Text(
+                name, style = TextStyle(
+                    fontSize = 20.sp, fontWeight = FontWeight.Bold
+                )
+            )
+        }
+        IconButton(onClick = {}) {
+            Icon(imageVector = Icons.Default.Search, contentDescription = "Search")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun HeaderViewPreview() {
+    HeaderView(modifier = Modifier)
+}
--- app/src/main/java/com/example/easyshop/ui/pages/HomePage.kt
+++ app/src/main/java/com/example/easyshop/ui/pages/HomePage.kt
@@ -1,12 +1,16 @@
 package com.example.easyshop.ui.pages
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.easyshop.ui.components.BannerView
+import com.example.easyshop.ui.components.HeaderView
 
 @Composable
 fun HomePage(modifier: Modifier = Modifier) {
@@ -15,6 +19,13 @@ fun HomePage(modifier: Modifier = Modifier) {
             .fillMaxSize()
             .padding(horizontal = 10.dp)
     ) {
-        Text("Welcome to Home Page")
+        HeaderView(modifier)
+        Spacer(Modifier.height(10.dp))
+        BannerView(modifier = modifier.height(200.dp))
     }
+
 }
+
+@Preview
+@Composable
+fun HomePagerPreview() {}
