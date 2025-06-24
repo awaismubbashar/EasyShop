b64c337b41654c2c62ec72fedf0b2a119afa3f21
Awais Mubbashar <awaismubbashar@gmail.com>
2025-06-24 09:00:47 +0500

    Update CartPage

--- app/src/main/java/com/example/easyshop/ui/pages/CartPage.kt
+++ app/src/main/java/com/example/easyshop/ui/pages/CartPage.kt
@@ -1,10 +1,70 @@
 package com.example.easyshop.ui.pages
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.easyshop.model.UserData
+import com.example.easyshop.ui.components.CartItemView
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.firestore
 
 @Composable
 fun CartPage(modifier: Modifier = Modifier) {
-    Text("CartPage")
+
+    val userData = remember {
+        mutableStateOf(UserData())
+    }
+
+    DisposableEffect (Unit) {
+        val listener = Firebase.firestore.collection("users")
+            .document(Firebase.auth.currentUser?.uid!!)
+            .addSnapshotListener { it, _ ->
+                if (it != null) {
+                    val result = it.toObject(UserData::class.java)
+                    result?.let { user ->
+                        userData.value = user
+                    }
+                }
+            }
+        onDispose {
+            listener.remove()
+        }
+    }
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(12.dp)
+    ) {
+        Text(
+            "Your Cart",
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        )
+        Spacer(Modifier.height(20.dp))
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            modifier = Modifier.fillMaxSize()
+        ) {
+            items(userData.value.cartItems.toList(), key = { it.first }) { (productId, quantity) ->
+                CartItemView(modifier, productId, quantity)
+            }
+        }
+    }
+
+
 }
